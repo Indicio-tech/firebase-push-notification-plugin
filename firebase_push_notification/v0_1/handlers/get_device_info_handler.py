7007826990df091f597139e9410d31492f7f305c
Char Howland <char@indicio.tech>
2022-06-01 22:36:27 -0600

    feat: (WIP) handlers
    Signed-off-by: Char Howland <char@indicio.tech>

---
+++ firebase_push_notification/v0_1/handlers/get_device_info_handler.py
@@ -0,0 +1,28 @@
+from aries_cloudagent.messaging.base_handler import (
+    BaseHandler,
+    BaseResponder,
+    RequestContext,
+)
+
+
+class GetDeviceInfoHandler(BaseHandler):
+    """Handler class for getting device info."""
+
+    async def handle(self, context: RequestContext, responder: BaseResponder):
+        """
+        Handle connection invitation.
+
+        Args:
+            context: Request context
+            responder: Responder callback
+        """
+
+        self._logger.debug(f"ConnectionInvitationHandler called with context {context}")
+        assert isinstance(context.message, GetDeviceInfo)
+
+        report = ConnectionProblemReport(
+            problem_code=ProblemReportReason.INVITATION_NOT_ACCEPTED,
+            explain="Connection invitations cannot be submitted via agent messaging",
+        )
+        # client likely needs to be using direct responses to receive the problem report
+        await responder.send_reply(report)
