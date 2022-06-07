7d3e80bfd907ddf4d7f46412ea2850e92a6e76ac
Char Howland <char@indicio.tech>
2022-06-07 10:41:25 -0600

    fix: update PROTOCOL_PACKAGE
    Signed-off-by: Char Howland <char@indicio.tech>

--- firebase_push_notification/v0_1/message_types.py
+++ firebase_push_notification/v0_1/message_types.py
@@ -4,9 +4,10 @@ from aries_cloudagent.protocols.didcomm_prefix import DIDCommPrefix
 
 PROTOCOL = "https://didcomm.org/push-notifications-fcm-android/1.0"
 
-PROTOCOL_PACKAGE = "firebase_push_notification.v0_1"
+PROTOCOL_PACKAGE = "firebase_push_notification.v0_1.message"
 
 
+# Message types
 SET_DEVICE_INFO = f"{PROTOCOL}/set-device-info"  # register device?
 GET_DEVICE_INFO = f"{PROTOCOL}/get-device-info"  # offer notification config?
 DEVICE_INFO = f"{PROTOCOL}/device-info"
@@ -15,9 +16,9 @@ PUSH_NOTIFICATION = f"{PROTOCOL}/push-notification"
 
 MESSAGE_TYPES = DIDCommPrefix.qualify_all(
     {
-        SET_DEVICE_INFO: "push_notifications_native.SetDeviceInfo",
-        GET_DEVICE_INFO: "push_notifications_native.GetDeviceInfo.",
-        DEVICE_INFO: "push_notifications_native.DeviceInfo.",
-        PUSH_NOTIFICATION: "push_notifications_native.PushNotification",
+        SET_DEVICE_INFO: f"{PROTOCOL_PACKAGE}.SetDeviceInfo",
+        GET_DEVICE_INFO: f"{PROTOCOL_PACKAGE}.GetDeviceInfo",
+        DEVICE_INFO: f"{PROTOCOL_PACKAGE}.DeviceInfo",
+        PUSH_NOTIFICATION: f"{PROTOCOL_PACKAGE}.PushNotification",
     }
 )
