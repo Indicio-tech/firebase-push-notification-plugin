ba8a1d2e41e8a35573b9c868fb159015d8053af5
Char Howland <char@indicio.tech>
2022-06-13 13:27:06 -0600

    fix: add setup function to firebase_push_notification/__init__.py
    Signed-off-by: Char Howland <char@indicio.tech>

--- firebase_push_notification/__init__.py
+++ firebase_push_notification/__init__.py
@@ -1,1 +1,44 @@
-__version__ = "0.1.0"
+__version__ = "1.0.0"
+import re
+import logging
+from aries_cloudagent.config.injection_context import InjectionContext
+from aries_cloudagent.core.event_bus import Event, EventBus
+from aries_cloudagent.core.profile import Profile
+# from aries_cloudagent.core.protocol_registry import ProtocolRegistry
+
+# from .v1_0.message_types import MESSAGE_TYPES
+
+LOGGER = logging.getLogger(__name__)
+
+# from .v1_0.messages import (
+#     set_device_info,
+#     get_device_info,
+#     device_info,
+#     push_notification,
+# )
+
+# MODULES = [
+#     set_device_info,
+#     get_device_info,
+#     device_info,
+#     push_notification,
+# ]
+
+from aries_cloudagent.core.plugin_registry import PluginRegistry
+
+async def setup(context: InjectionContext, plugin_registry: PluginRegistry = None):
+    """Setup the plugin."""
+    bus = context.inject(EventBus)
+    assert bus
+    LOGGER.debug("Registering Firebase plugin")
+
+    # for mod in MODULES:
+    #     await mod.setup(context)
+
+    # LOGGER.debug("Here are the message types: ", type(MESSAGE_TYPES), MESSAGE_TYPES)
+
+    # if not plugin_registry:
+    #     plugin_registry = context.inject(PluginRegistry)
+    #     # LOGGER.debug("This is the plugin registry: ", plugin_registry)
+
+    # plugin_registry.register_package("firebase_push_notification.v1_0")
