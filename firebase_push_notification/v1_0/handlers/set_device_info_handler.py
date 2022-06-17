b5c50341661090bd4a5a940dd24ce21fbfb50dae
Char Howland <char@indicio.tech>
2022-06-16 18:03:48 -0600

    feat: save DeviceRecord in SetDeviceInfoHandler
    Signed-off-by: Char Howland <char@indicio.tech>

--- firebase_push_notification/v1_0/handlers/set_device_info_handler.py
+++ firebase_push_notification/v1_0/handlers/set_device_info_handler.py
@@ -3,8 +3,11 @@ from aries_cloudagent.messaging.base_handler import (
     BaseResponder,
     RequestContext,
 )
+from aries_cloudagent.core.event_bus import Event
+from aries_cloudagent.core.profile import ProfileSession
 
 from ..messages.set_device_info import SetDeviceInfo
+from ..models.device_record import DeviceRecord
 
 
 class SetDeviceInfoHandler(BaseHandler):
@@ -18,9 +21,23 @@ class SetDeviceInfoHandler(BaseHandler):
             context: Request context
             responder: Responder callback
         """
-
-        self._logger.debug(f"ConnectionInvitationHandler called with context {context}")
+        
+        self._logger.debug(f"SetDeviceInfoHandler called with context {context}")
         assert isinstance(context.message, SetDeviceInfo)
+
+        device_record = DeviceRecord(
+            device_token=context.message.device_token,
+        )
+        self._logger.debug(f"device record! {device_record}")
+        self._logger.debug(f"context.message! {context.message}")
+
+        async with context.profile.session() as session:
+            await device_record.save(
+                session,
+                reason="Save device info"
+            )
+        
+        # TODO: add problem report
 
         # report = ConnectionProblemReport(
         #     problem_code=ProblemReportReason.INVITATION_NOT_ACCEPTED,
