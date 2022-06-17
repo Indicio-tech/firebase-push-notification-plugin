b1613c23dd5e7d1f395b66c9d4650d27a02d034d
Char Howland <char@indicio.tech>
2022-06-16 18:02:05 -0600

    fix: GetDeviceInfoHandler to use DeviceRecord.query()
    Signed-off-by: Char Howland <char@indicio.tech>

--- firebase_push_notification/v1_0/handlers/get_device_info_handler.py
+++ firebase_push_notification/v1_0/handlers/get_device_info_handler.py
@@ -6,8 +6,11 @@ from aries_cloudagent.messaging.base_handler import (
     RequestContext,
 )
 
+from firebase_push_notification.v1_0.messages.device_info import DeviceInfo
+
 from ..messages.get_device_info import GetDeviceInfo
 from ..manager import PushNotificationManager
+from ..models.device_record import DeviceRecord
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,17 +26,21 @@ class GetDeviceInfoHandler(BaseHandler):
             context: Request context
             responder: Responder callback
         """
-        profile = context.profile
-        LOGGER.debug("GetDeviceInfoHandler: %s", context.message_delivery.message_id)
+
         self._logger.debug(f"GetDeviceInfoHandler called with context {context}")
         assert isinstance(context.message, GetDeviceInfo)
 
-        credential_manager = PushNotificationManager(profile)
-        get_device_info = GetDeviceInfo()
-        credential_manager.send_device_info(get_device_info)
+        session = await context.session()
+        records = await DeviceRecord.query(session, {})
 
+        self._logger.debug(f"Logged records: {records}")
+
+        device_info = DeviceInfo(device_token=records[0].device_token)
+        device_info.assign_thread_from(context.message)
+        await responder.send_reply(device_info)
 
         # TODO: add problem report
+
         # report = ConnectionProblemReport(
         #     problem_code=ProblemReportReason.INVITATION_NOT_ACCEPTED,
         #     explain="Connection invitations cannot be submitted via agent messaging",
