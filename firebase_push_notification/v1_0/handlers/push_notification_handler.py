de68380bb1973a8bee1e359ff9d215723d8fd74f
Char Howland <char@indicio.tech>
2022-06-29 17:49:52 -0600

    feat: add device_token to PushNotificationHandler
    Signed-off-by: Char Howland <char@indicio.tech>

--- firebase_push_notification/v1_0/handlers/push_notification_handler.py
+++ firebase_push_notification/v1_0/handlers/push_notification_handler.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from typing import Optional, cast
+from typing import Optional
 
 import requests
 from aries_cloudagent.messaging.base_handler import (BaseHandler,
@@ -12,8 +12,18 @@ from ..messages.push_notification_ack import PushNotificationAck
 
 LOGGER = logging.getLogger(__name__)
 
+
 class PushNotificationHandler(BaseHandler):
     """Handler class for push notification."""
+
+    def __init__(
+        self,
+        *,
+        device_token: Optional[str] = None,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.device_token = device_token
 
     async def handle(self, context: RequestContext, responder: BaseResponder):
         """
@@ -26,26 +36,34 @@ class PushNotificationHandler(BaseHandler):
 
         self._logger.debug(f"PushNotificationHandler called with context {context}")
         assert isinstance(context.message, PushNotification)
-        #send push notification to fb for device
         LOGGER.info("Firebase push notification")
         configs = context.profile.settings["plugin_config"].get("firebase_plugin", {}) # possible bug with incorrect profile
         firebase_server_token = configs.get("firebase_server_token")
-        device_token = configs.get("device_token")
+        if not self.device_token:
+            self.device_token = configs.get("device_token")
+
+        # TODO: lookup from device registry using recipient key
+        payload = {
+            "message_id": context.message.message_id
+        }
         headers = {"Content-Type": "application/json", "Authorization": f"key={firebase_server_token}"}
         body = {
             "notification": {
                 "title": "Sending push notification from ACA-Py",
                 "body": "Test push notification",
             },
-            "to": device_token,
-            "priority": "high",
-            "data": {},
+            "to": self.device_token,
+            "priority": context.message.priority,
+            "data": payload,
         }
+        LOGGER.info(f"Push notification handler body {body}")
+        LOGGER.info(f"Push notification handler headers {headers}")
         response = requests.post(
             "https://fcm.googleapis.com/fcm/send", headers=headers, data=json.dumps(body)
         )
+        LOGGER.info(f"Push notification response {response}.")
         try:
-            LOGGER.info(f"Sending firebase notification {{}}.")
+            LOGGER.info(f"In handler sending firebase notification {payload}.")
         except Exception:
             LOGGER.exception("Firebase producer failed to send notification")
 
