84ffa688ff75b444533bbdcc056ec34214d279cd
Char Howland <char@indicio.tech>
2022-06-29 17:46:00 -0600

    feat: admin API endpoint for push notification
    Signed-off-by: Char Howland <char@indicio.tech>

--- firebase_push_notification/v1_0/routes.py
+++ firebase_push_notification/v1_0/routes.py
@@ -3,14 +3,23 @@ import re
 import requests
 import json
 from typing import Optional, cast
+from aiohttp import web
+from aiohttp_apispec import (
+    docs,
+    request_schema,
+    response_schema,
+)
 
-from aries_cloudagent.connections.models.conn_record import ConnRecord
 from aries_cloudagent.core.event_bus import Event, EventBus, EventWithMetadata
-from aries_cloudagent.core.profile import Profile, ProfileSession
-from aries_cloudagent.core.util import SHUTDOWN_EVENT_PATTERN, STARTUP_EVENT_PATTERN
-from aries_cloudagent.messaging.agent_message import AgentMessage
+from aries_cloudagent.core.profile import Profile
 from aries_cloudagent.messaging.responder import BaseResponder
-from aries_cloudagent.protocols.connections.v1_0.manager import ConnectionManager
+from aries_cloudagent.messaging.request_context import RequestContext
+from aries_cloudagent.admin.request_context import AdminRequestContext
+
+from .messages.push_notification import PushNotificationSchema
+from .messages.push_notification_ack import PushNotificationAckSchema
+from .messages.push_notification import PushNotification
+from .handlers.push_notification_handler import PushNotificationHandler
 
 LOGGER = logging.getLogger(__name__)
 
@@ -18,6 +27,7 @@ LOGGER = logging.getLogger(__name__)
 def register_events(event_bus: EventBus):
     """Register to handle events."""
     LOGGER.info("Firebase, subscribe to all events!")
+    event_bus.subscribe(re.compile(re.compile(".*")), handle_event)
 
 
 RECORD_RE = re.compile(r"acapy::record::([^:]*)(?:::(.*))?")
@@ -67,10 +77,52 @@ async def handle_event(profile: Profile, event: EventWithMetadata):
         "priority": "high",
         "data": payload,
     }
+    LOGGER.info(f"Routes body {body}")
+    LOGGER.info(f"Routes headers {headers}")
     response = requests.post(
         "https://fcm.googleapis.com/fcm/send", headers=headers, data=json.dumps(body)
     )
     try:
-        LOGGER.info(f"Sending firebase notification {payload}.")
+        LOGGER.info(f"In routes sending firebase notification {payload}.")
     except Exception:
         LOGGER.exception("Firebase producer failed to send notification")
+
+
+
+async def register(app: web.Application):
+    app.add_routes(
+        [
+            web.post("/push-notification/{firebase_server_token}{device_token}", push_notification),
+        ]
+    )
+
+
+@docs(
+    tags=["pushnotification"],
+    summary="Send a push notification",
+)
+@request_schema(PushNotificationSchema())
+@response_schema(PushNotificationAckSchema(), 200, description="")
+async def push_notification(request: web.BaseRequest):
+
+    body = await request.json()
+    handler = PushNotificationHandler(
+        device_token = body.get("device_token")
+    )
+
+    context: AdminRequestContext = request["context"]
+    profile = context.profile
+    request_context = RequestContext(profile=profile)
+    request_context.message = PushNotification(
+        message_id="placeholder",
+        recipient_key="placeholder",
+        priority="default",
+    )
+    responder = context.injector.inject(BaseResponder)
+
+    await handler.handle(
+        context=request_context,
+        responder=responder
+    )
+
+    return web.json_response()
