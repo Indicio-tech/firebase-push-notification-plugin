d50c9b1485b89450a5ae7cc7525753931cab8146
Char Howland <char@indicio.tech>
2022-07-28 11:34:11 -0600

    fix: rename int test file
    Signed-off-by: Char Howland <char@indicio.tech>

---
+++ int/tests/test_device_info.py
@@ -0,0 +1,76 @@
+import pytest
+import logging
+import os
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.asyncio
+async def test_set_device_info(echo, echo_connection):
+    """Test for setting device info. SetDeviceInfo message responds
+    with DeviceInfo message."""
+
+    device_token = os.getenv("DEVICE_TOKEN")
+
+    async with echo.session(echo_connection) as session:
+        await echo.send_message_to_session(
+            session,
+            {
+                "@type": "https://didcomm.org/push-notifications-fcm/1.0/set-device-info",
+                "device_token": device_token,
+            },
+        )
+
+        device_info = await echo.get_message(
+            echo_connection,
+            session=session,
+            msg_type=(
+                "https://didcomm.org/push-notifications-fcm/1.0/device-info"
+            )
+        )
+
+    assert device_info["device_token"] == device_token
+
+
+
+@pytest.mark.asyncio
+async def test_get_device_info(echo, echo_connection):
+    """Test for setting and getting device info.GetDeviceInfo message responds
+    with DeviceInfo message."""
+
+    device_token = os.getenv("DEVICE_TOKEN")
+
+    async with echo.session(echo_connection) as session:
+        await echo.send_message_to_session(
+            session,
+            {
+                "@type": "https://didcomm.org/push-notifications-fcm/1.0/set-device-info",
+                "device_token": device_token,
+            },
+        )
+
+        await echo.get_message(
+            echo_connection,
+            session=session,
+            msg_type=(
+                "https://didcomm.org/push-notifications-fcm/1.0/device-info"
+            )
+        )
+
+        await echo.send_message_to_session(
+            session,
+            {
+                "@type": "https://didcomm.org/push-notifications-fcm/1.0/get-device-info",
+                "thread_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            }
+        )
+
+        device_info = await echo.get_message(
+            echo_connection,
+            session=session,
+            msg_type=(
+                "https://didcomm.org/push-notifications-fcm/1.0/device-info"
+            )
+        )
+
+    assert device_info["device_token"] == device_token
--- tests/test_fire.py
+++
@@ -1,5 +0,0 @@
-from firebase_push_notification import __version__
-
-
-def test_version():
-    assert __version__ == "1.0.0"
