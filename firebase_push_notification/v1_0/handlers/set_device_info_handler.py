2ee3be8044d107c718c730be613250315d7f58dd
Char Howland <char@indicio.tech>
2022-08-05 18:04:26 -0600

    feat: method to check that connection is mediated
    Signed-off-by: Char Howland <char@indicio.tech>

--- firebase_push_notification/v1_0/handlers/set_device_info_handler.py
+++ firebase_push_notification/v1_0/handlers/set_device_info_handler.py
@@ -3,6 +3,7 @@ from aries_cloudagent.messaging.base_handler import (
     BaseResponder,
     RequestContext,
 )
+from aries_cloudagent.protocols.coordinate_mediation.v1_0.models.mediation_record import MediationRecord
 
 from ..messages.set_device_info import SetDeviceInfo
 from ..messages.device_info import DeviceInfo
@@ -24,7 +25,7 @@ class SetDeviceInfoHandler(BaseHandler):
         self._logger.debug(f"SetDeviceInfoHandler called with context {context}")
         assert isinstance(context.message, SetDeviceInfo)
 
-        device_info = await self.set_device_handler(
+        device_info = await self.set_device_info_handler(
             context,
             device_token=context.message.device_token,
             connection_id=context.connection_record.connection_id,
@@ -51,3 +52,13 @@ class SetDeviceInfoHandler(BaseHandler):
             device_info = DeviceInfo(device_token=records[0].device_token)
         
         return device_info
+
+    async def verify_mediated_connection(self, context: RequestContext, connection_id):
+        """
+        Verify that the connection between agent and relay/mediator is mediated.
+        """
+        session = await context.session()
+        mediation_record = await MediationRecord.retrieve_by_connection_id(session, connection_id)
+        assert mediation_record.state == "granted"
+
+        return mediation_record
